0f196bae801a35d661ff3bfd255e5d715dbba139
ian-itworkserv <ian@itworkserv.com>
2020-10-07 20:19:08 +0800

    Clean up

--- resources/views/user/under-maintenance/index.blade.php
+++ resources/views/user/under-maintenance/index.blade.php
@@ -4,20 +4,6 @@
     }
 
     div#under-maintenance {
-        /* height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #dd2032;
-    color: #ffffff;    
-    font-family: 'Montserrat', sans-serif;
-    font-size: 40px;
-    font-weight: 900;
-    letter-spacing: 4px;
-    line-height: 1.2;
-    margin-bottom: 20px;
-    text-transform: uppercase; */
-        background-image: url('img/surface.jpg');
         background-size: contain;
         background-repeat: no-repeat;
         background-position: center center;
