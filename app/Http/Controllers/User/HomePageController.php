a28c797b98e744fc48add975a5afe85fdb0d492a
repoitworkserv <repo.itworkserv@gmail.com>
2020-10-13 23:43:11 +0800

    home color swatches

--- app/Http/Controllers/User/HomePageController.php
+++ app/Http/Controllers/User/HomePageController.php
@@ -45,7 +45,45 @@ class HomePageController extends Controller
         $user_type = Auth::user()['users_type_id'];
         $userBrands = UserBrands::where('user_id',$uid)->pluck('brand_id')->all();
         $brands = Brand::get();
-        return view('user.home.index', compact('Page', 'Post', 'Product','uid', 'user_type','userBrands','brands'));
+        $colors = array(
+            array(
+                'name' => 'Whites </br>& Neutrals',
+                'color' => '#F6F7F2'
+            ),
+            array(
+                'name' => 'Greys',
+                'color' => '#373E42'
+            ),
+            array(
+                'name' => 'Browns',
+                'color' => '#B39F94'
+            ),
+            array(
+                'name' => 'Purples',
+                'color' => '#7E7999'
+            ),
+            array(
+                'name' => 'Blue',
+                'color' => '#0045C7'
+            ),
+            array(
+                'name' => 'Greens',
+                'color' => '#9DBFAF'
+            ),
+            array(
+                'name' => 'Yellows',
+                'color' => '#FAE196'
+            ),
+            array(
+                'name' => 'Oranges',
+                'color' => '#CC5327'
+            ),
+            array(
+                'name' => 'Reds',
+                'color' => '#A8312F'
+            ),           
+        );        
+        return view('user.home.index', compact('Page', 'Post', 'Product','uid', 'user_type','userBrands','brands','colors'));
     }
 
 }
